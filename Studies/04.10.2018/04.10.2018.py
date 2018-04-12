3a9936a08da4e4056617c28c8bff6e6563abd32f
IgorBozhok <31074368+IgorBozhok@users.noreply.github.com>
2018-04-12 21:21:20 +0300

    Add files via upload

--- Studies/04.10.2018/04.10.2018.py
+++ Studies/04.10.2018/04.10.2018.py
@@ -1,9 +1,9 @@
-#game 'Black jack'
+# game 'Black jack'
 
 from os import system
 from time import sleep
 from random import *
-#from random import shuffle
+from random import shuffle
 
 
 def test(choice):
@@ -20,43 +20,47 @@ def test1(karta):
 
     if karta == 'A':
         return 11
-    elif karta == 'J' or karta == 'D' or karta == 'K':
+    elif karta == 'J' or karta == 'Q' or karta == 'K':
         return 10
     else:
-        return karta
+        return int(karta)
+def sum(sum1,karta):
+    return int(sum1 + karta)
 
-
-#main
+# main
+sum1 = 0
+sum2 = 0
 print('                 Привет! Сейчас поиграем в игру "Black jack "!')
+name1 = str(input("Введите Ваше имя : "))
 print("")
 print("1) Начать игру.")
 print("2) Если ты не азартный человек, лучше и не начинай ;-)")
 print("")
 
-#Начало
-while choice != 1 and choice != 2:
-    choice = test(choice)
+# Начало
+
+choice = ""
+while choice != '1' and choice != '2':
+    choice = input("Ваш выбор :")
+
 
 
 #Вводим имя.
+if choice == '1':
 
-if choice == 1:
 
-    name1 = str(input("Введите Ваше имя : "))
     print("Начинаем",name1)
     system('cls')
     koloda = [6,7,8,9,10,'J','Q','K','A']*4
     shuffle(koloda)
-    sum1=0
-    sum2=0
     KarPlayer = []
     KarDiler = []
 # 1 Ход
+
     for q in range (0,2):
         karta=koloda.pop()
         KarPlayer.append(karta)
-        karta1=test1(karta)
-        sum1 +=karta1
+        sum1 =sum(sum1,test1(karta))
     if sum1==22:
         print("Ваши карты", name1)
         print(KarPlayer, "Black jack")
@@ -67,8 +71,7 @@ if choice == 1:
     for q in range (0,2):
         karta=koloda.pop()
         KarDiler.append(karta)
-        karta2=test1(karta)
-        sum2 +=karta2
+        sum2 = sum(sum2, test1(karta))
     if sum2 == 22:
         print("Карты Diler")
         print(KarDiler, " = ", sum2)
@@ -76,51 +79,40 @@ if choice == 1:
         print("Карты Diler")
         print(KarDiler, " = ", sum2)
     print (name1, ", если берем карту, то жмем 1")
-    print("Если нет, то жмем 2")
+    print("Если нет, то жмем 0")
+    choice1 = ""
+    while choice1 != '1' and choice1 != '0':
+        choice1 = input("Ваш выбор :")
+    flag = 0
+    while choice1 == '1' and flag == 0:
+        karta = koloda.pop()
+        KarPlayer.append(karta)
+        sum1 = sum(sum1, test1(karta))
+        print("Ваши карты", name1)
+        print(KarPlayer, " = ", sum1)
 
-# играем дальше
-    choice
-    while choice !=2:
-        while choice != 1 and choice != 2:
-            choice = test(choice)
-        if choice == 1:
-            karta = koloda.pop()
-            KarPlayer.append(karta)
-            karta1 = test1(karta)
-            sum1 += karta1
-            if sum1 == 22:
-                print("Ваши карты", name1)
-                print(KarPlayer, "Black jack")
-            else:
-                print("Ваши карты", name1)
-                print(KarPlayer, " = ", sum1)
-            karta = koloda.pop()
-            KarDiler.append(karta)
-            karta2 = test1(karta)
-            sum2 += karta2
-            if sum2 == 22:
-                print("Карты Diler")
-                print(KarDiler, " = ", sum2)
-            else:
-                print("Карты Diler")
-                print(KarDiler, " = ", sum2)
+        karta = koloda.pop()
+        KarDiler.append(karta)
+        sum2 = sum(sum2, test1(karta))
+        print("Карты Diler")
+        print(KarDiler, " = ", sum2)
         print(name1, ", если берем карту, то жмем 1")
-        print("Если нет, то жмем 2")
-        while choice != 1 and choice != 2:
-            choice = test(choice)
+        print("Если нет, то жмем 0")
+        choice2 = ""
+        while choice2 != '1' and choice2 != '0':
+            choice2 = input("Ваш выбор :")
+            if choice2 == '1':
+                print("Играем дальше")
+            elif choice2 == '0':
+                flag = 1
+                break
+        if flag == 1:
+            break
 
-        else:
-             quit(1)
 
 
-
-
-##     print(koloda)
-
-# shuffle(koloda)
-# print(koloda)
-# karta = koloda.pop()
-# print(karta)
-
-else:
+# elif choice == 0:
+#     if sum1>sum2:
+#     print (name1, "Вы выйграли!")
+elif choice == '0':
     quit(1)
